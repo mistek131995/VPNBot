1b62cbc6ebcc896891a7371d595fb0a8b32d9418
Юрий Кузнецов <omega131995@outlook.com>
2023-11-19 17:34:27 +0300

    Удалена ссылка на Window приложение

--- VpnBotApi/Worker/TelegramBot/Handler/MessageHandler/DownloadApp/Handler.cs
+++ VpnBotApi/Worker/TelegramBot/Handler/MessageHandler/DownloadApp/Handler.cs
@@ -16,8 +16,7 @@ namespace VpnBotApi.Worker.TelegramBot.Handler.MessageHandler.DownloadApp
                 new InlineKeyboardButton[]
                 {
                     InlineKeyboardButton.WithUrl("Android", "https://play.google.com/store/apps/details?id=com.v2ray.ang"),
-                    InlineKeyboardButton.WithUrl("IOS", "https://apps.apple.com/us/app/streisand/id6450534064"),
-                    InlineKeyboardButton.WithUrl("Windows", "https://github.com/MatsuriDayo/nekoray/releases")
+                    InlineKeyboardButton.WithUrl("IOS", "https://apps.apple.com/us/app/streisand/id6450534064")
                 }
             });
 
