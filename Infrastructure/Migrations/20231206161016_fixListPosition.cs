97c2c5810624f38047b351e33e31047046391e0d
Юрий Кузнецов <omega131995@outlook.com>
2023-12-06 19:10:38 +0300

    Дополнение к пред. коммиту

---
+++ Infrastructure/Migrations/20231206161016_fixListPosition.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixListPosition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_AccessPositionId",
+                table: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Accesses_VpnServerId",
+                table: "Accesses");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_AccessPositionId",
+                table: "Payments",
+                column: "AccessPositionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accesses_VpnServerId",
+                table: "Accesses",
+                column: "VpnServerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_AccessPositionId",
+                table: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Accesses_VpnServerId",
+                table: "Accesses");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_AccessPositionId",
+                table: "Payments",
+                column: "AccessPositionId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accesses_VpnServerId",
+                table: "Accesses",
+                column: "VpnServerId",
+                unique: true);
+        }
+    }
+}
