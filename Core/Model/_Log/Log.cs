0e007138aa0a58a6105c7a742c16e87b823433c7
Юрий Кузнецов <omega131995@outlook.com>
2023-12-08 07:54:50 +0300

    Конфликт с Serilog

---
+++ Core/Model/_Log/Log.cs
@@ -0,0 +1,12 @@
+﻿using Core.Common;
+
+namespace Core.Model.Log
+{
+    public class Log : IAggregate
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public string Level { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
