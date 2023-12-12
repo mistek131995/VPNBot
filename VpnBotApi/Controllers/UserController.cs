b49d69450b04643bc0c9da35d203ea04289daf22
Юрий Кузнецов <omega131995@outlook.com>
2023-12-12 20:14:17 +0300

    Экшен для получения доступа

--- VpnBotApi/Controllers/UserController.cs
+++ VpnBotApi/Controllers/UserController.cs
@@ -27,6 +27,14 @@ namespace VpnBotApi.Controllers
         }
 
         [HttpGet]
+        public async Task<JsonResult> GetAccess()
+        {
+            var access = "vless://4a2cc04a-2184-4311-be9c-15216ac09461@2.59.183.140:443?type=tcp&security=reality&fp=chrome&pbk=K3aNlmMC_2WU39eLkUGcp4arcNpc8ze1aKTnpcS-tAc&sni=yahoo.com&sid=ab2cc97b&spx=%2F#subscribe-access-vpn2";
+
+            return Json(access);
+        }
+
+        [HttpGet]
         [Authorize]
         public async Task<JsonResult> GetIndex()
         {
