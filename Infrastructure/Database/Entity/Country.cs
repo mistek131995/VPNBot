1e3e0245f19eaec0bd1d61c182d990342424d4d3
mistek131995 <mistek131995@gmail.com>
2024-01-13 05:55:05 +0300

    Тег в таблице страны

--- Infrastructure/Database/Entity/Country.cs
+++ Infrastructure/Database/Entity/Country.cs
@@ -6,6 +6,7 @@ namespace Infrastructure.Database.Entity
     {
         [Key]
         public int Id { get; set; }
+        public int Tag {  get; set; }
         public string Name { get; set; }
 
         public List<VpnServer> VpnServers { get; set; }
---
+++ Infrastructure/Migrations/20240113025430_tagInCountry.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class tagInCountry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Tag",
+                table: "Countries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tag",
+                table: "Countries");
+        }
+    }
+}
