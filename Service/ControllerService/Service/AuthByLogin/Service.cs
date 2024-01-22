1e972b8b234c3cc76947b6fd7d9346e5fa9e618f
mistek131995 <mistek131995@gmail.com>
2024-01-22 07:37:33 +0300

    Проверка пользователя перед входом

--- Service/ControllerService/Service/AuthByLogin/Service.cs
+++ Service/ControllerService/Service/AuthByLogin/Service.cs
@@ -12,6 +12,12 @@ namespace Service.ControllerService.Service.AuthByLogin
             var user = await repositoryProvider.UserRepository.GetByLoginAndPasswordAsync(request.Login, request.Password) ??
                 throw new Exception("Пользователь с таким логином и паролем не найден.");
 
+            if(user.Sost == UserSost.Blocked)
+                throw new Exception("Ваш аккаунт заблокирован.");
+
+            if (user.Sost == UserSost.NotActive)
+                throw new Exception("Ваш аккаунт не активирован. Проверьте электронную почту, письмо может быть в папке 'Спам'.");
+
             return Helper.CreateJwtToken(user, configuration);
         }
     }
