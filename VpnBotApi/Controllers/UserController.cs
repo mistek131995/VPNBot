d5c05554ae460f5eb7551726ab578ec7facbe20f
mistek131995 <mistek131995@gmail.com>
2024-01-22 05:54:24 +0300

    Главная гостевая страница

--- VpnBotApi/Controllers/UserController.cs
+++ VpnBotApi/Controllers/UserController.cs
@@ -1,15 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 
-using GetLinkAuth = Service.ControllerService.Service.GetLinkAuth;
 using GetIndex = Service.ControllerService.Service.GetIndex;
 using GetAccessPositions = Service.ControllerService.Service.GetAccessPositions;
 
 using Register = Service.ControllerService.Service.Register;
 using Activation = Service.ControllerService.Service.ActivateUser;
-using LoginByLink = Service.ControllerService.Service.AuthByLink;
-using LoginByLogin = Service.ControllerService.Service.AuthByLogin;
-using SetLoginAndPassword = Service.ControllerService.Service.SetLoginAndPassword;
+using Login = Service.ControllerService.Service.AuthByLogin;
 using ChangePassword = Service.ControllerService.Service.ChangePassword;
 using Application.ControllerService.Common;
 
@@ -40,14 +37,6 @@ namespace VpnBotApi.Controllers
         }
 
         [HttpGet]
-        public async Task<JsonResult> LoginByLink([FromQuery] GetLinkAuth.Request request)
-        {
-            var response = await dispatcher.GetService<GetLinkAuth.Result, GetLinkAuth.Request>(request);
-
-            return Json(response);
-        }
-
-        [HttpGet]
         public async Task<string> GetAccess()
         {
             var access = "vless://4a2cc04a-2184-4311-be9c-15216ac09461@2.59.183.140:443?type=tcp&security=reality&fp=chrome&pbk=K3aNlmMC_2WU39eLkUGcp4arcNpc8ze1aKTnpcS-tAc&sni=yahoo.com&sid=ab2cc97b&spx=%2F#subscribe-access-vpn2";
@@ -71,25 +60,9 @@ namespace VpnBotApi.Controllers
         }
 
         [HttpPost]
-        public async Task<JsonResult> LoginByLink([FromBody] LoginByLink.Request command)
+        public async Task<JsonResult> Login([FromBody] Login.Request request)
         {
-            var response = await dispatcher.GetService<string, LoginByLink.Request>(command);
-
-            return Json(response);
-        }
-
-        [HttpPost]
-        public async Task<JsonResult> LoginByLogin([FromForm] LoginByLogin.Request request)
-        {
-            var response = await dispatcher.GetService<string, LoginByLogin.Request>(request);
-
-            return Json(response);
-        }
-
-        [HttpPost]
-        public async Task<JsonResult> SetLoginAndPassword([FromBody] SetLoginAndPassword.Request query)
-        {
-            var response = await dispatcher.GetService<bool, SetLoginAndPassword.Request>(query);
+            var response = await dispatcher.GetService<string, Login.Request>(request);
 
             return Json(response);
         }
