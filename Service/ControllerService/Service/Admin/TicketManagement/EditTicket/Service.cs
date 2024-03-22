bf705862a217e0193d9615e3dac4e572c669b4bd
Юрий Кузнецов <omega131995@outlook.com>
2024-03-22 12:43:47 +0600

    Исправление ошибки регистрации + еще что-то старое

---
+++ Service/ControllerService/Service/Admin/TicketManagement/EditTicket/Service.cs
@@ -0,0 +1,17 @@
+﻿using Application.ControllerService.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.ControllerService.Service.Admin.TicketManagement.EditTicket
+{
+    internal class Service : IControllerService<Request, ViewModel>
+    {
+        public Task<ViewModel> HandlingAsync(Request request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
