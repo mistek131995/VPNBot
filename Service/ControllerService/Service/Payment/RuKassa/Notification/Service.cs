f71ff6773e313e7e3fbc808059069123717d808b
Юрий Кузнецов <omega131995@outlook.com>
2024-04-01 23:29:49 +0600

    Настройка рукассы

--- Service/ControllerService/Service/Payment/RuKassa/Notification/Service.cs
+++ Service/ControllerService/Service/Payment/RuKassa/Notification/Service.cs
@@ -1,6 +1,8 @@
 ﻿using Application.ControllerService.Common;
 using Core.Common;
 using Service.ControllerService.Common;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Service.ControllerService.Service.Payment.RuKassa.Notification
 {
@@ -8,12 +10,36 @@ namespace Service.ControllerService.Service.Payment.RuKassa.Notification
     {
         public async Task<bool> HandlingAsync(Request request)
         {
-            var signature = Signature.GenerateSignature(request.Query, "f1bcf17bb8a0a91966e6bb55b20e6761");
+            var signature = CalculateHmacSha256("f1bcf17bb8a0a91966e6bb55b20e6761", request.Query);
 
             Console.WriteLine(signature);
             Console.WriteLine(request.Signature);
 
+            Console.WriteLine("---------------");
+            Console.WriteLine(request.Query);
+
             return true;
+        }
+
+        public static string CalculateHmacSha256(string key, string message)
+        {
+            // Ключ и сообщение должны быть преобразованы в массив байтов
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+            // Использование класса HMACSHA256 для вычисления HMAC
+            using (HMACSHA256 hmac = new HMACSHA256(keyBytes))
+            {
+                byte[] hashBytes = hmac.ComputeHash(messageBytes);
+
+                // Преобразование результата из массива байтов в шестнадцатеричную строку
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in hashBytes)
+                {
+                    sb.AppendFormat("{0:x2}", b);
+                }
+                return sb.ToString();
+            }
         }
     }
 }
