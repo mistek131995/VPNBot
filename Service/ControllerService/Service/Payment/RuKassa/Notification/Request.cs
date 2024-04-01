d2bf3a4220703f991ba016a1e42e21f1ced2bc0a
Юрий Кузнецов <omega131995@outlook.com>
2024-04-01 22:55:09 +0600

    Настройка рукассы

--- Service/ControllerService/Service/Payment/RuKassa/Notification/Request.cs
+++ Service/ControllerService/Service/Payment/RuKassa/Notification/Request.cs
@@ -4,13 +4,13 @@ namespace Service.ControllerService.Service.Payment.RuKassa.Notification
 {
     public class Request : IRequest<bool>
     {
-        public int id { get; set; }
-        public int order_id { get; set; }
-        public float amount { get; set; }
+        public int? id { get; set; }
+        public int? order_id { get; set; }
+        public float? amount { get; set; }
         public float in_amount { get; set; }
         public string? data { get; set; }
-        public DateTime createdDateTime { get; set; }
-        public string status { get; set; }
+        public string? createdDateTime { get; set; }
+        public string? status { get; set; }
         public string? Signature { get; set; }
     }
 }
