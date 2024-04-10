36f636e12c3c47afebbe4aca586aa4478cb53ec0
Юрий Кузнецов <omega131995@outlook.com>
2024-04-10 20:12:44 +0600

    Обновил некоторые пакеты

--- Service/ControllerService/Service/UploadFile/Service.cs
+++ Service/ControllerService/Service/UploadFile/Service.cs
@@ -1,6 +1,5 @@
 ﻿using Application.ControllerService.Common;
 using Core.Common;
-using Renci.SshNet;
 using File = Core.Model.File.File;
 
 namespace Service.ControllerService.Service.UploadFile
@@ -9,43 +8,7 @@ namespace Service.ControllerService.Service.UploadFile
     {
         public async Task<bool> HandlingAsync(Request request)
         {
-            var file = await repositoryProvider.FileRepository.GetByTagAsync(request.Tag);
 
-            var settings = await repositoryProvider.SettingsRepositroy.GetSettingsAsync();
-
-            var connectionInfo = new ConnectionInfo(
-                "185.215.186.224", 
-                settings.SSHServerLogin, 
-                new PasswordAuthenticationMethod(
-                    settings.SSHServerLogin, 
-                    settings.SSHServerPassword)
-                );
-
-            using var client = new SftpClient(connectionInfo);
-            using var stream = new MemoryStream(request.Data);
-
-            client.Connect();
-
-            if (!client.Exists($"{settings.FileBasePath}/{request.Tag}"))
-                client.CreateDirectory($"{settings.FileBasePath}/{request.Tag}");
-
-            //FileBasePath - /home/build/wwwroot/files
-            client.UploadFile(stream, $"{settings.FileBasePath}/{request.Tag}/{request.Name}", null);
-
-            client.Disconnect();
-
-            if(file == null)
-            {
-                await repositoryProvider.FileRepository.AddAsync(new File(0, request.Tag, request.Name, request.ContentType, request.Version));
-            }
-            else
-            {
-                file.Name = request.Name;
-                file.ContentType = request.ContentType;
-                file.Version = request.Version;
-
-                repositoryProvider.FileRepository.UpdateAsync(file);
-            }
 
             return true;
         }
