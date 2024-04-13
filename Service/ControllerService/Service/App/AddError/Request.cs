9182dd273aef2109f976ff77a80e6ad62d845e15
Юрий Кузнецов <omega131995@outlook.com>
2024-04-13 18:50:05 +0600

    Доработка логирования

--- Service/ControllerService/Service/App/AddError/Request.cs
+++ Service/ControllerService/Service/App/AddError/Request.cs
@@ -9,7 +9,7 @@ namespace Service.ControllerService.Service.App.AddError
 {
     public class Request : IRequest<bool>
     {
-        public string Message { get; set; }
-        public string StackTrace { get; set; }
+        public string? Message { get; set; }
+        public string? StackTrace { get; set; }
     }
 }
