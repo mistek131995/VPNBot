5beba7f041176098f58db55eea68cbfd51eff13b
Юрий Кузнецов <omega131995@outlook.com>
2024-04-25 20:11:19 +0600

    Отключены платежи ниже 300руб

--- VpnBotApi/Controllers/PaymentController.cs
+++ VpnBotApi/Controllers/PaymentController.cs
@@ -10,9 +10,6 @@ using FreeKassaGetLink = Service.ControllerService.Service.Payment.FreeKassa.Get
 using CreateRuKassaLink = Service.ControllerService.Service.Payment.RuKassa.CreateLink;
 using RuKassaNotification = Service.ControllerService.Service.Payment.RuKassa.Notification;
 
-using CreateLavaLink = Service.ControllerService.Service.Payment.Lava.CreateLink;
-using LavaNotification = Service.ControllerService.Service.Payment.Lava.Notification;
-
 using GetPaymentPositions = Service.ControllerService.Service.Payment.GetPaymentPositions;
 using ApplyPromoCode = Service.ControllerService.Service.Payment.ApplyPromoCode;
 
@@ -76,7 +73,6 @@ namespace VpnBotApi.Controllers
             return Json(response);
         }
 
-
         [HttpPost]
         public async Task<string> Notification([FromForm] FreeKassaNotification.Request request)
         {
@@ -86,19 +82,6 @@ namespace VpnBotApi.Controllers
                 return "YES";
             else
                 return "NO";
-        }
-
-        [HttpGet]
-        [Authorize]
-        public async Task<JsonResult> CreateLavaLink(int id)
-        {
-            var response = await dispatcher.GetService<string, CreateLavaLink.Request>(new CreateLavaLink.Request()
-            {
-                Id = id,
-                UserId = int.Parse(HttpContext.User.Claims.FirstOrDefault(x => x.Type == "id").Value)
-            });
-
-            return Json(response);
         }
 
         [HttpGet]
@@ -135,25 +118,6 @@ namespace VpnBotApi.Controllers
             var response = await dispatcher.GetService<bool, PayOkNotification.Request>(request);
 
             return Json(response);
-        }
-
-
-        [HttpPost]
-        public async Task LavaNotification([FromBody] LavaNotification.Request request)
-        {
-            request.Signature = Request.Headers.Authorization.ToString();
-
-            Console.WriteLine("Начало выполнения------------------------------------");
-            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(request));
-            //Console.WriteLine("Заголовок------------------------------------");
-            //Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(Request.Headers));
-            //Console.WriteLine("Подпись------------------------------------");
-            //Console.WriteLine(Request.Headers.Authorization.ToString());
-            //Console.WriteLine("Тело строкой------------------------------------");
-            //Console.WriteLine(Request.Body.ToString());
-            Console.WriteLine("Конец выполнения------------------------------------");
-
-            await dispatcher.GetService<bool, LavaNotification.Request>(request);
         }
 
         public async Task RuKassaNotification()
