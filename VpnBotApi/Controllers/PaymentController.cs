130dcf1e493bbb7ec2c0e980b891b93e6b05aa7a
Юрий Кузнецов <omega131995@outlook.com>
2024-04-30 21:37:11 +0600

    Test

--- VpnBotApi/Controllers/PaymentController.cs
+++ VpnBotApi/Controllers/PaymentController.cs
@@ -102,7 +102,7 @@ namespace VpnBotApi.Controllers
         }
 
         [HttpPost]
-        public async Task<bool> CryptoCloudNotification([FromBody]string? status, [FromBody]string? invoice_id)
+        public async Task<bool> CryptoCloudNotification()
         {
 
             //var requestContent = "";
