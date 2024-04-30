189a11bd565ffdf6da3d178ea5fb04053ad81ea3
Юрий Кузнецов <omega131995@outlook.com>
2024-04-30 20:17:45 +0600

    Подтверждение оплаты CryptoCloud

--- Service/ControllerService/Service/Payment/CryptoCloud/Notification/Service.cs
+++ Service/ControllerService/Service/Payment/CryptoCloud/Notification/Service.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Application.ControllerService.Common;
+using Core.Common;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,13 @@ using System.Threading.Tasks;
 
 namespace Service.ControllerService.Service.Payment.CryptoCloud.Notification
 {
-    internal class Service
+    internal class Service(IRepositoryProvider repositoryProvider) : IControllerService<Request, bool>
     {
+        public async Task<bool> HandlingAsync(Request request)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(request));
+
+            return true;
+        }
     }
 }
