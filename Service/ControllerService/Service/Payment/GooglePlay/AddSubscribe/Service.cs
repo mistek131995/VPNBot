75f826872e972412a641e335c9bcff1775be5c40
Юрий Кузнецов <omega131995@outlook.com>
2024-05-19 22:21:03 +0600

    Добавление токена и типа подписки в пользователя

--- Service/ControllerService/Service/Payment/GooglePlay/AddSubscribe/Service.cs
+++ Service/ControllerService/Service/Payment/GooglePlay/AddSubscribe/Service.cs
@@ -1,70 +1,56 @@
 ﻿using Application.ControllerService.Common;
 using Core.Common;
-using Google.Apis.Auth.OAuth2;
-using Newtonsoft.Json;
 using Service.ControllerService.Common;
-using System.Net.Http.Headers;
 
 namespace Service.ControllerService.Service.Payment.GooglePlay.AddSubscribe
 {
     internal class Service(IRepositoryProvider repositoryProvider, Serilog.ILogger logger) : IControllerService<Request, bool>
     {
+        public record AutoRenewingPlan(bool autoRenewEnabled);
+
+        public record LineItem(string productId, DateTime expiryTime, AutoRenewingPlan autoRenewingPlan, OfferDetails offerDetails);
+
+        public record OfferDetails(string basePlanId);
+
+        public class Root(string kind, DateTime startTime, string regionCode, string subscriptionState, string latestOrderId, string acknowledgementState, List<LineItem> lineItems);
+
         public async Task<bool> HandlingAsync(Request request)
         {
             var user = await repositoryProvider.UserRepository.GetByIdAsync(request.UserId)
                 ?? throw new HandledException("Пользователь не найден", true);
 
-            // Путь к файлу ключа JSON сервисного аккаунта
-            string jsonKeyFilePath = "wwwroot/keyfile.json";
-            // Область доступа, необходимая для вашего запроса
-            string[] scopes = new string[] { "https://www.googleapis.com/auth/androidpublisher" };
+            user.SubscribeToken = request.SubscribeToken;
+            user.SubscribeType = Core.Model.User.SubscribeType.GooglePlay;
 
-            GoogleCredential credential = GoogleCredential.FromFile(jsonKeyFilePath)
-            .CreateScoped(scopes);
+            await repositoryProvider.UserRepository.UpdateAsync(user);
 
-            var accessToken = await credential.UnderlyingCredential.GetAccessTokenForRequestAsync();
+            logger.Information($"Пользователю {user.Login} успешно добавлен токен Google Play.");
 
-            var httpClient = new HttpClient();
+            //// Путь к файлу ключа JSON сервисного аккаунта
+            //string jsonKeyFilePath = "wwwroot/keyfile.json";
+            //// Область доступа, необходимая для вашего запроса
+            //string[] scopes = new string[] { "https://www.googleapis.com/auth/androidpublisher" };
 
-            //var response = await httpClient.PostAsync("https://accounts.google.com/o/oauth2/token", content);
+            //GoogleCredential credential = GoogleCredential.FromFile(jsonKeyFilePath)
+            //.CreateScoped(scopes);
+
+            //var accessToken = await credential.UnderlyingCredential.GetAccessTokenForRequestAsync();
+
+            //var httpClient = new HttpClient();
+
+            //httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            //var response = await httpClient.GetAsync($"https://androidpublisher.googleapis.com/androidpublisher/v3/applications/com.lockvpnandroidapp/purchases/subscriptionsv2/tokens/{request.SubscribeToken}");
             //if (!response.IsSuccessStatusCode)
             //{
             //    var errorResponse = await response.Content.ReadAsStringAsync();
             //    logger.Information(errorResponse);
             //    throw new ApplicationException("Не удалось получить токен.");
             //}
+            //var data = await response.Content.ReadAsStringAsync();
 
-            //var responseString = await response.Content.ReadAsStringAsync();
-            //var tokenResponse = JsonConvert.DeserializeObject<TokenResponse>(responseString);
-
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-            var response = await httpClient.GetAsync($"https://androidpublisher.googleapis.com/androidpublisher/v3/applications/com.lockvpnandroidapp/purchases/subscriptionsv2/tokens/{request.SubscribeToken}");
-            if (!response.IsSuccessStatusCode)
-            {
-                var errorResponse = await response.Content.ReadAsStringAsync();
-                logger.Information(errorResponse);
-                throw new ApplicationException("Не удалось получить токен.");
-            }
-            var data = await response.Content.ReadAsStringAsync();
-
-            logger.Information(data);
+            //logger.Information(data);
 
             return true;
-        }
-
-        public class TokenResponse
-        {
-            [JsonProperty("access_token")]
-            public string AccessToken { get; set; }
-
-            [JsonProperty("token_type")]
-            public string TokenType { get; set; }
-
-            [JsonProperty("expires_in")]
-            public int ExpiresIn { get; set; }
-
-            [JsonProperty("refresh_token")]
-            public string RefreshToken { get; set; }
         }
     }
 }
--- Service/ControllerService/Service/Payment/GooglePlay/Notification/Service.cs
+++ Service/ControllerService/Service/Payment/GooglePlay/Notification/Service.cs
@@ -1,16 +1,16 @@
 ﻿using Application.ControllerService.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Core.Common;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Service.ControllerService.Service.Payment.GooglePlay.Notification
 {
-    internal class Service : IControllerService<Request, bool>
+    internal class Service(IRepositoryProvider repositoryProvider, Serilog.ILogger logger) : IControllerService<Request, bool>
     {
         public async Task<bool> HandlingAsync(Request request)
         {
+            var base64EncodedBytes = Convert.FromBase64String(request.message.data);
+            logger.Information(Encoding.UTF8.GetString(base64EncodedBytes));
+
             return true;
         }
     }
