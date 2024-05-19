c8d0a3b54a1b86e91c3c0fe8f350000aab53f91e
Юрий Кузнецов <omega131995@outlook.com>
2024-05-19 10:53:07 +0600

    Тест аутентификации AndroidPublisher

--- Service/ControllerService/Service/Payment/GooglePlay/AddSubscribe/Service.cs
+++ Service/ControllerService/Service/Payment/GooglePlay/AddSubscribe/Service.cs
@@ -1,6 +1,7 @@
 ﻿using Application.ControllerService.Common;
 using Core.Common;
-using Newtonsoft.Json.Linq;
+using Google.Apis.AndroidPublisher.v3;
+using Google.Apis.Auth.OAuth2;
 using Service.ControllerService.Common;
 
 namespace Service.ControllerService.Service.Payment.GooglePlay.AddSubscribe
@@ -9,15 +10,26 @@ namespace Service.ControllerService.Service.Payment.GooglePlay.AddSubscribe
     {
         public async Task<bool> HandlingAsync(Request request)
         {
-            var user = await repositoryProvider.UserRepository.GetByIdAsync(request.UserId) 
+            var user = await repositoryProvider.UserRepository.GetByIdAsync(request.UserId)
                 ?? throw new HandledException("Пользователь не найден", true);
 
             var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync($"https://androidpublisher.googleapis.com/androidpublisher/v3/applications/com.lockvpnandroidapp/purchases/subscriptionsv2/tokens/{request.SubscribeToken}");
-            response.EnsureSuccessStatusCode();
-            var data = await response.Content.ReadAsStringAsync();
 
-            logger.Information(data);
+            UserCredential credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                new ClientSecrets
+                {
+                    ClientId = "354549970311-bi5ordl9jsbsbl874vob4bq1ahr6r98k.apps.googleusercontent.com",
+                    ClientSecret = "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"
+                },
+                new[] { AndroidPublisherService.Scope.Androidpublisher },
+                "user",
+                CancellationToken.None);
+
+            //var response = await httpClient.GetAsync($"https://androidpublisher.googleapis.com/androidpublisher/v3/applications/com.lockvpnandroidapp/purchases/subscriptionsv2/tokens/{request.SubscribeToken}");
+            //response.EnsureSuccessStatusCode();
+            //var data = await response.Content.ReadAsStringAsync();
+
+            logger.Information(credential.Token.AccessToken);
 
             return true;
         }
