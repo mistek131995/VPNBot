725724a1de422f8636165d483468b4ace39d60a8
Юрий Кузнецов <omega131995@outlook.com>
2024-05-19 11:18:29 +0600

    Тест аутентификации AndroidPublisher

--- Service/ControllerService/Service/Payment/GooglePlay/AddSubscribe/Service.cs
+++ Service/ControllerService/Service/Payment/GooglePlay/AddSubscribe/Service.cs
@@ -2,6 +2,7 @@
 using Core.Common;
 using Google.Apis.AndroidPublisher.v3;
 using Google.Apis.Auth.OAuth2;
+using Newtonsoft.Json;
 using Service.ControllerService.Common;
 
 namespace Service.ControllerService.Service.Payment.GooglePlay.AddSubscribe
@@ -15,23 +16,47 @@ namespace Service.ControllerService.Service.Payment.GooglePlay.AddSubscribe
 
             var httpClient = new HttpClient();
 
-            UserCredential credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-                new ClientSecrets
-                {
-                    ClientId = "354549970311-bi5ordl9jsbsbl874vob4bq1ahr6r98k.apps.googleusercontent.com",
-                    ClientSecret = "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"
-                },
-                new[] { AndroidPublisherService.Scope.Androidpublisher },
-                "user",
-                CancellationToken.None);
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "authorization_code"),
+                new KeyValuePair<string, string>("code", "4/0AdLIrYc25eVCPJDONWVxp3XkTZvOQleCec7h6sNwH2i1dHrRJFL6YwmzAINRG746AWzjjA"),
+                new KeyValuePair<string, string>("client_id", "354549970311-bi5ordl9jsbsbl874vob4bq1ahr6r98k.apps.googleusercontent.com\r\n"),
+                new KeyValuePair<string, string>("client_secret", "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"),
+                new KeyValuePair<string, string>("redirect_uri", "https://lockvpn.me")
+            });
+
+            var response = await httpClient.PostAsync("https://accounts.google.com/o/oauth2/token", content);
+            if (!response.IsSuccessStatusCode)
+            {
+                logger.Information(response.ReasonPhrase);
+                throw new ApplicationException("Не удалось получить токен.");
+            }
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var tokenResponse = JsonConvert.DeserializeObject<TokenResponse>(responseString);
 
             //var response = await httpClient.GetAsync($"https://androidpublisher.googleapis.com/androidpublisher/v3/applications/com.lockvpnandroidapp/purchases/subscriptionsv2/tokens/{request.SubscribeToken}");
             //response.EnsureSuccessStatusCode();
             //var data = await response.Content.ReadAsStringAsync();
 
-            logger.Information(credential.Token.AccessToken);
+            logger.Information(responseString);
 
             return true;
+        }
+
+        public class TokenResponse
+        {
+            [JsonProperty("access_token")]
+            public string AccessToken { get; set; }
+
+            [JsonProperty("token_type")]
+            public string TokenType { get; set; }
+
+            [JsonProperty("expires_in")]
+            public int ExpiresIn { get; set; }
+
+            [JsonProperty("refresh_token")]
+            public string RefreshToken { get; set; }
         }
     }
 }
